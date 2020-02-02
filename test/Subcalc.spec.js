b26c8db42be43c5abb541d15b84db40eac60f8c9
Cooper Labinger <clabinger@gmail.com>
2020-02-02 01:06:13 +0000

    Remove sample items

--- test/Subcalc.spec.js
+++ test/Subcalc.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import Subcalc from '@/components/Subcalc.vue'
 
-describe('Logo', () => {
+describe('Subcalc', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
+    const wrapper = mount(Subcalc)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
